045228443ef47b8ebb05580752018d1d3ebb586c
Stephen Roe <93727098+stephengroe@users.noreply.github.com>
2024-08-12 18:47:07 -0700

    Build basic to-do list app

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,62 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [task, setTask] = useState('');
+  const [taskList, setTaskList] = useState<string[]>([]);
+
+  function addTask(e) {
+    e.preventDefault();
+
+    const newTaskList = [
+      task,
+      ...taskList,
+    ];
+
+    setTaskList(newTaskList);
+    setTask('');
+  }
+  
+  function deleteTask(taskToDelete: string) {
+    const newTaskList = taskList.filter(task => {
+      return task !== taskToDelete;
+    });
+
+    setTaskList(newTaskList);
+  }
 
   return (
     <>
+      <h1>Basic To-Do List</h1>
+
+      <form onSubmit={addTask}>
+        <input type='text' value={task} onChange={(e) => setTask(e.target.value)} />
+        <button type='submit'>Add Task</button>
+      </form>
+
+      <ul>
+        {taskList.map((task, index) => 
+          <li key={index}>
+            {task}
+            <button onClick={() => deleteTask(task)}>X</button>
+          </li>
+        )}
+      </ul>
     </>
   )
 }
+
+
+// Current task
+// List of tasks
+// Add handler
+// Delete handler
+// form
+  // Input w/ state for task
+  // Submit button—append to end of list
+// ul
+  // map over list
+  // key = index
+  // text = task
+  // delete button
 
 export default App
