5d8a2734c3d5101e1f0fa0ef6087a1678e019f94
Stephen Roe <93727098+stephengroe@users.noreply.github.com>
2024-08-17 15:23:02 -0700

    Add list of ratings

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,53 @@
-import { StrictMode } from 'react'
+import { StrictMode, useState } from 'react'
 import { createRoot } from 'react-dom/client'
 import StarRating from './StarRating';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <StarRating numberOfStars={2} />
-  </StrictMode>,
+    <RatingsList />
+  </StrictMode>
 )
+
+
+type rating = {
+  id: number,
+  numberOfStars: number,
+}
+
+function RatingsList() {
+  const [ratings, setRatings] = useState<rating[]>([
+    {
+      id: 1,
+      numberOfStars: 3,
+    },
+    {
+      id: 2,
+      numberOfStars: 2,
+    },
+    {
+      id: 3,
+      numberOfStars: 1,
+    },
+    {
+      id: 4,
+      numberOfStars: 5,
+    }
+  ]);
+
+  function updateRatings(updatedRating: rating) {
+    const newRatings = [
+      ...ratings,
+      updatedRating
+    ];
+
+    setRatings(newRatings);
+  }
+
+  return (
+    <>
+    {ratings.map(rating => 
+      <StarRating key={rating.id} numberOfStars={rating.numberOfStars} />
+    )}
+    </>
+  );
+}
