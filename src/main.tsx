31949ffeb3c6c4c06366d40e50ad66221ecd1c1a
Stephen Roe <93727098+stephengroe@users.noreply.github.com>
2024-08-18 14:12:21 -0700

    Add Root route

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,20 @@
-import { StrictMode, useState } from 'react'
-import { createRoot } from 'react-dom/client'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import {
+  createBrowserRouter,
+  RouterProvider
+} from 'react-router-dom';
+import './index.css';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <div className='p-10'>Hellow, World!</div>
+  }
+]);
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+    <RouterProvider router={router}></RouterProvider>
   </StrictMode>
 )
